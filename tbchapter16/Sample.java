fac971e4bdf70edbcff63d23c5f59f8b59cb0c87
DavinTech <148042975+DavinTech@users.noreply.github.com>
2023-11-09 18:29:56 +0900

    課題提出20231109

---
+++ tbchapter16/Sample.java
@@ -0,0 +1,16 @@
+package tbchapter16;
+
+public class Sample {
+
+	public static String getColor(int rgb) {
+		switch (rgb) {
+		case 1:
+			return "赤";
+		case 2:
+			return "緑";
+		case 3:
+			return "青";
+		}
+		throw new IllegalArgumentException();
+	}
+}
